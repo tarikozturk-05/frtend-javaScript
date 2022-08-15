848da2a60c61b1bf01ba07a2a76bc2ca8c38d3ee
TarikOzturk <109246134+tarikozturk-05@users.noreply.github.com>
2022-08-16 01:45:48 +0300

    Add files via upload

---
+++ js4/app.js
@@ -0,0 +1,142 @@
+// const sayi = Number(prompt("bir sayı giriniz"))
+// if(sayi > 0){
+//     console.log(`${sayi} pozitiftir`);
+// }
+// console.log(sayi, typeof sayi) ;
+
+// --------------
+
+// const n1 = +prompt("sayi 1")
+// const n2 = +prompt("sayi 2")
+// const n3 = +prompt("sayi 3")
+
+// if(n1 >= n2 && n1 >= n3){
+//     console.log(`${n1} en büyük sayıdır`)
+// }else if(n2 >= n1 && n2 >= n3){
+//     console.log(`${n2} en büyük sayıdır`)
+// }else if(n3 >= n1 && n3 >= n2){
+//     console.log(`${n3} en büyüktür`)
+// }  else{
+//     console.log(`lütfen geçerli bir değer giriniz`)
+// }
+
+// ?------------------------ödevödevvv---------
+
+
+// let sayı1 = +prompt(`sayı giriniz`)
+// let sayı2 = +prompt(`sayı giriniz`)
+// let işlem = prompt(`işlem giriniz(+,-,/,*)`)
+
+// if(işlem == "+"){
+//     console.log(sayı1+sayı2)
+// }
+// else if(işlem == "-"){
+//     console.log(sayı1-sayı2)
+// }
+// else if(işlem == "/"){
+//     console.log(sayı1/sayı2)
+// }
+// else if(işlem == "*"){
+//     console.log(sayı1*sayı2)
+// }
+// else{
+//     console.log(`lütfen işlem giriniz(+,-,/,*)`)
+// }
+
+
+// ----------------2-----------
+
+// let day = prompt(`enter day`);
+
+// switch (day.toLowerCase()) {
+//     case  `monday`:   
+
+//     case  `tuesday`:
+
+//     case `wednesday`:
+
+//     case `thursday`:
+//         console.log(`In class dersleri`)
+//         break;
+
+//     case  `friday`:
+//         console.log(`teamWork`)
+//         break;
+//     case  `saturday`:
+//         console.log(`InClass and workshop`)
+//         break;
+//     case `sunday`:
+//         console.log(`Tatil`)
+//         break;
+
+//     default:
+//         console.log(`Hatalı gün girdiniz`)
+//         break;
+// }
+
+
+
+// ----------------------3-------
+
+
+// const asgari = 5500
+// let maas = +prompt(`lütfen maasınızı girin`)
+
+// let zamlımaas = maas >= asgari ? (maas*1.1) : (maas*1.5)
+// console.log(`Yeni maaşınız : ${zamlımaas.toFixed(2)}`)
+
+// -------ifififif-----
+
+
+// const asgari = 5500
+// let maas = +prompt(`lütfen maasınızı girin`)
+
+// let zamlımaas = maas >= asgari ? (maas*1.1) : (maas*1.5)
+// console.log(`Yeni maaşınız : ${zamlımaas.toFixed(2)}`)
+
+// const asgari =(`5500`)
+// let maas = +prompt(`maası giriniz`)
+// if(maas>=asgari){
+//     console.log(`zamlı maas ${maas*1.1}`)
+// }
+// else if(maas <=asgari){
+//     console.log(`zamsız maas ${maas*1.5}`)
+// }
+// else{
+//     console.log(`zam yok`)
+// }
+
+// -----------------------4-------
+
+// const asgari = 5500
+// let gelir = +prompt("gelirinizi giriniz")
+// let gider = +prompt("giderinizi giriniz")
+// let fark = (gelir - gider)
+// console.log(fark >= asgari ? "Kredi alabilirsiniz" : "Kredi alamazsınız")
+
+// -------2.yolll--------
+
+
+// console.log((+prompt(`Gelir miktarınızı giriniz`))-(+prompt(`Gider miktarınızı giriniz`)) >= 5500 ? `Kredi Verilebilir 🤑` : `Kredi Verilemez 🥺`)
+
+// -----------5---------
+
+
+// let not = +prompt(`not giriniz`);
+// if(not >= 90){
+//     console.log(`Notunuz : ${not} AA`)
+// }else if(not >= 75){
+//     console.log(`Notunuz : ${not} BA`)
+// }else if( not >= 65){
+//     console.log(`Notunuz : ${not} BB`)
+// }else if( not >= 45){
+//     console.log(`Notunuz : ${not} CC`)
+// }else if( not >= 25){
+//     console.log(`Notunuz : ${not} DD`)
+// }else if( not >= 0){
+//     console.log(`Notunuz : ${not} FF`)
+// }else {
+//     console.log(`Düzgün bir değer giriniz`)
+// }
+
+
